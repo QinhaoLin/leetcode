77fe64daf3c50e2de6121676cb955f02ce25a999
linqinhao <976147727@qq.com>
2020-04-09 17:10:09 +0800

    t46

---
+++ src/main/java/easy/t46/Main.java
@@ -0,0 +1,40 @@
+package easy.t46;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @fileName: Main
+ * @description:
+ * @create: 2020-04-09 16:43
+ */
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+        System.out.println("new Main().permute() = " + new Main().permute(nums));
+    }
+
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        int[] visited = new int[nums.length];
+        backtrack(res, nums, new ArrayList<Integer>(), visited);
+        return res;
+    }
+
+    private void backtrack(List<List<Integer>> res, int[] nums, ArrayList<Integer> tmp, int[] visited) {
+        if (tmp.size() == nums.length) {
+            res.add(new ArrayList<>(tmp));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (visited[i] == 1) {
+                continue;
+            }
+            visited[i] = 1;
+            tmp.add(nums[i]);
+            backtrack(res, nums, tmp, visited);
+            visited[i] = 0;
+            tmp.remove(tmp.size() - 1);
+        }
+    }
+}
