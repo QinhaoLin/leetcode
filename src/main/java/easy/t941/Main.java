8daa6ce461c2cab22f96936208a677a7b82c8dc9
linqinhao <976147727@qq.com>
2020-04-18 13:57:28 +0800

    打卡 t941

---
+++ src/main/java/easy/t941/Main.java
@@ -0,0 +1,33 @@
+package easy.t941;
+
+/**
+ * @fileName: Main
+ * @description:
+ * @create: 2020-04-18 10:30
+ */
+public class Main {
+    public static void main(String[] args) {
+        int[] A = {0, 3, 2, 1};
+        System.out.println("new Main().validMountainArray() = " + new Main().validMountainArray(A));
+    }
+
+    public boolean validMountainArray(int[] A) {
+        if (A == null || A.length < 3) {
+            return false;
+        }
+        int i = 0;
+        // 上山
+        while (i < A.length - 1 && A[i] < A[i + 1]) {
+            i++;
+        }
+        // 下山
+        while (i < A.length - 1 && A[i] > A[i + 1]) {
+            i++;
+        }
+        if (i == A.length - 1) {
+            return true;
+        }
+        return false;
+    }
+
+}
