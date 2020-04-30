d2c800695f1e3ba5149a71368a98e2ff9a2ee5b2
linqinhao <976147727@qq.com>
2020-04-30 16:19:00 +0800

    打卡 t633

---
+++ src/main/java/easy/t633/Main.java
@@ -0,0 +1,30 @@
+package easy.t633;
+
+/**
+ * @fileName: Main
+ * @description:
+ * @create: 2020-04-30 16:14
+ */
+public class Main {
+    public static void main(String[] args) {
+        int c = 5;
+        System.out.println("new Main().judgeSquareSum() = " + new Main().judgeSquareSum(c));
+    }
+
+
+    public boolean judgeSquareSum(int c) {
+        int left = 0;
+        int right = (int) Math.sqrt(c);
+        while (left <= right) {
+            int sum = left * left + right + right;
+            if (sum == c) {
+                return true;
+            } else if (sum > c) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+        return false;
+    }
+}
---
+++ src/main/java/medium/t309_/Main.java
@@ -0,0 +1,18 @@
+package medium.t309_;
+
+/**
+ * @fileName: Main
+ * @description:
+ * @create: 2020-04-30 15:44
+ */
+public class Main {
+    public static void main(String[] args) {
+        int[] prices = {1, 2, 3, 0, 2};
+        System.out.println("new Main().maxProfit() = " + new Main().maxProfit(prices));
+    }
+
+    public int maxProfit(int[] prices) {
+
+        return 0;
+    }
+}
