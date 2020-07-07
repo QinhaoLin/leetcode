c76e13a69be30ba640d5bc1d2f2d8edd37b4332b
linqinhao <976147727@qq.com>
2020-07-07 18:11:47 +0800

    t389

---
+++ src/main/java/easy/t389/Main.java
@@ -0,0 +1,31 @@
+package easy.t389;
+
+/**
+ * @fileName: Main
+ * @description:
+ * @create: 2020-07-07 17:56
+ */
+public class Main {
+    public static void main(String[] args) {
+        String s = "abcd";
+        String t = "abcde";
+        System.out.println("new Main().findTheDifference() = " + String.valueOf(new Main().findTheDifference(s, t)));
+    }
+
+
+    public char findTheDifference(String s, String t) {
+        int[] cont = new int[26];
+        for (int i = 0; i < t.length(); i++) {
+            cont[t.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            cont[s.charAt(i) - 'a']--;
+        }
+        for (int i = 0; i < cont.length; i++) {
+            if (cont[i] > 0) {
+                return (char) (i + 'a');
+            }
+        }
+        return 'a';
+    }
+}
