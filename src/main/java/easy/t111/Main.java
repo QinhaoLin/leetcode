891de41e9b828a9b854130d6b5875f8f49c574b1
linqinhao <976147727@qq.com>
2021-06-08 14:15:51 +0800

    t111

---
+++ src/main/java/easy/t111/Main.java
@@ -0,0 +1,56 @@
+package easy.t111;
+
+/**
+ * @fileName: Main
+ * @description:
+ * @create: 2021-06-08 11:48
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 深度优先搜索
+     * 首先可以想到使用深度优先搜索的方法，遍历整棵树，记录最小深度。
+     * 对于每一个非叶子节点，我们只需要分别计算其左右子树的最小叶子节点深度。这样就将一个大问题转化为了小问题，可以递归地解决该问题。
+     *
+     * @param root
+     * @return
+     */
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null && root.right == null) {
+            return 1;
+        }
+        int min_depth = Integer.MAX_VALUE;
+        if (root.left != null) {
+            min_depth = Math.min(minDepth(root.left), min_depth);
+        }
+        if (root.right != null) {
+            min_depth = Math.min(minDepth(root.right), min_depth);
+        }
+        return min_depth + 1;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
