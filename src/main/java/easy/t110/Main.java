74a4d96c2b731a600f154028180b40fd83446359
linqinhao <976147727@qq.com>
2021-06-08 15:26:59 +0800

    t110

---
+++ src/main/java/easy/t110/Main.java
@@ -0,0 +1,48 @@
+package easy.t110;
+
+/**
+ * @fileName: Main
+ * @description:
+ * @create: 2021-06-08 14:40
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public boolean isBalanced(TreeNode root) {
+        return height(root) >= 0;
+    }
+
+    private int height(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int lh = height(root.left);
+        int rh = height(root.right);
+        if (lh >= 0 && rh >= 0 && Math.abs(lh - rh) <= 1) {
+            return Math.max(lh, rh) + 1;
+        } else {
+            return -1;
+        }
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
