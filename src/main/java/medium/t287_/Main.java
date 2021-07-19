3826b2e2f015b9f422ad8564fc73e1b43e8026b1
linqinhao <976147727@qq.com>
2021-07-19 18:26:13 +0800

    t300

---
+++ src/main/java/medium/t287_/Main.java
@@ -0,0 +1,18 @@
+package medium.t287_;
+
+/**
+ * @fileName: Main
+ * @description:
+ * @create: 2021-07-19 17:27
+ */
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 3, 4, 2, 2};
+        System.out.println("new Main().findDuplicate(nums) = " + new Main().findDuplicate(nums));
+    }
+
+    public int findDuplicate(int[] nums) {
+
+        return 0;
+    }
+}
---
+++ src/main/java/medium/t300/Main.java
@@ -0,0 +1,38 @@
+package medium.t300;
+
+/**
+ * @fileName: Main
+ * @description:
+ * @create: 2021-07-19 17:55
+ */
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = new int[]{10, 9, 2, 5, 3, 7, 101, 18};
+        System.out.println("new Main().lengthOfLIS(nums) = " + new Main().lengthOfLIS(nums));
+    }
+
+    /**
+     * dp
+     *
+     * @param nums
+     * @return
+     */
+    public int lengthOfLIS(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int[] dp = new int[nums.length];
+        dp[0] = 1;
+        int maxans = 1;
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            maxans = Math.max(maxans, dp[i]);
+        }
+        return maxans;
+    }
+}
