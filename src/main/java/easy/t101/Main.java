ba77603e78cde958e4ad15731b84a55a451c9c39
linqinhao <976147727@qq.com>
2021-11-22 17:58:49 +0800

    t101

---
+++ src/main/java/easy/t101/Main.java
@@ -0,0 +1,48 @@
+package easy.t101;
+
+/**
+ * @fileName: Main
+ * @description:
+ * @create: 2021-11-22 16:52
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return cmp(root.left, root.right);
+    }
+
+    private boolean cmp(TreeNode node1, TreeNode node2) {
+        if (node1 == null && node2 == null) {
+            return true;
+        }
+        if (node1 == null || node2 == null || node1.val != node2.val) {
+            return false;
+        }
+        return cmp(node1.left, node2.right) && cmp(node1.right, node2.left);
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
