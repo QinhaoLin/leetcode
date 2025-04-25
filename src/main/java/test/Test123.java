7ed95cda8df729f72caacc788b62486591155b1d
linqinhao <harlan_lin@sekorm.com>
2025-04-25 13:48:21 +0800

    第一次 commit

--- src/main/java/test/Test123.java
+++ src/main/java/test/Test123.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class Test123 {
     private final static List<String> LIST = Arrays.asList("应用", "PN", "封装", "包装");
 
+    // 第一次 commit
     public static void main(String[] args) {
         String test = "应用";
         boolean contains = LIST.contains(test);
