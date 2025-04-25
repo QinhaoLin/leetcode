c2ab8418b4d2a41a2a5e868b4797a7ecf0acc216
linqinhao <harlan_lin@sekorm.com>
2025-04-25 13:58:19 +0800

    2

--- src/main/java/test/Test123.java
+++ src/main/java/test/Test123.java
@@ -10,6 +10,7 @@ public class Test123 {
     // 第二次 commit
     // 第三次 commit
     // 1
+    // 2
     public static void main(String[] args) {
         String test = "应用";
         boolean contains = LIST.contains(test);
