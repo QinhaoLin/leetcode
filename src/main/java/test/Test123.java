f41907ded070a2bdc152c43ad565d784af6333f4
linqinhao <harlan_lin@sekorm.com>
2025-04-25 13:48:41 +0800

    第三次 commit

--- src/main/java/test/Test123.java
+++ src/main/java/test/Test123.java
@@ -7,6 +7,8 @@ public class Test123 {
     private final static List<String> LIST = Arrays.asList("应用", "PN", "封装", "包装");
 
     // 第一次 commit
+    // 第二次 commit
+    // 第三次 commit
     public static void main(String[] args) {
         String test = "应用";
         boolean contains = LIST.contains(test);
