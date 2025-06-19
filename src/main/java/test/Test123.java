a1dae425aa044d52d3f2ed2eec8e9b5a7fbd0746
linqinhao <harlan_lin@sekorm.com>
2025-06-19 18:36:00 +0800

    test

--- src/main/java/test/Test123.java
+++ src/main/java/test/Test123.java
@@ -1,7 +1,9 @@
 package test;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Test123 {
     private final static List<String> LIST = Arrays.asList("应用", "PN", "封装", "包装");
@@ -14,8 +16,12 @@ public class Test123 {
     // 4
     // 333
     public static void main(String[] args) {
-        String test = "应用";
-        boolean contains = LIST.contains(test);
-        System.out.println("contains = " + contains);
+//        String test = "应用";
+//        boolean contains = LIST.contains(test);
+//        System.out.println("contains = " + contains);
+
+        Map<Object, Object> map = new HashMap<>();
+        map.put("test", null);
+        System.out.println("map = " + map);
     }
 }
